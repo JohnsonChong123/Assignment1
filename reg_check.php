4bc33ea6613116ffe4973a1f654bfa747f567182
VonYuan <voonyuan11@gmail.com>
2021-05-05 18:29:58 +0800

    update

--- reg_check.php
+++ reg_check.php
@@ -14,6 +14,7 @@
         
         if (file_exists($_POST['input_nric'] . '.txt')){
             echo '<h3>You are already registered!</h3>';
+            echo '<a href ="home.html">Back to home here</a>';
         }
         else{
             echo '<h3>You have not registered!</h3><br>';
